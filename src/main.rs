fc2dcbc4e46126d7437af079e5325f6f5ddb473f
Herman Gohn <gohnherman@gmail.com>
2025-04-19 14:43:07 +0200

    lets get this

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,95 @@
+use core::ffi::c_char;
+
 use anyhow::{bail, Result};
-use std::fs::File;
+
+use std::fs;
+use std::io;
 use std::io::prelude::*;
+
+/// A dot-command has the structure:
+///  - It must begin with its "." at the left margin with no preceding whitespace.
+///  - It must be entirely contained on a single input line.
+///  - It cannot occur in the middle of an ordinary SQL statement, thus it cannot occur at a continuation prompt
+///  - There is no comment syntax for dot-commands
+pub struct Command<'a> {
+    pub name: &'a str,
+}
+
+pub const COMMAND_COUNT: usize = 1;
+pub const COMMANDS: [Command<'_>; COMMAND_COUNT] = [Command { name: "dbinfo" }];
+
+const HEADER_STRING_SIZE: usize = 16;
+const HEADER_RESERVED_SIZE: usize = 20;
+#[derive(Debug)]
+#[repr(C)]
+pub struct DatabaseHeader {
+    /// The header string: "SQLite format 3\000"
+    pub header_string: [c_char; HEADER_STRING_SIZE],
+    /// The database page size in bytes.
+    ///
+    /// Must be a power of two between 512 and 32768 inclusive,
+    /// or the value 1 representing a page size of 65536.
+    pub page_size: u16,
+    /// File format write version.
+    ///
+    /// 1 for Legacy; 2 for WAL.
+    pub file_format_write_version: u8,
+    /// File format read version.
+    ///
+    /// 1 for Legacy; 2 for WAL.
+    pub file_format_read_version: u8,
+    /// Bytes of "unused" reserved space at the end of each page.
+    ///
+    /// Usually 0
+    pub reserved_page_tail_bytes: u8,
+    /// Must be 64
+    pub maximum_embedded_payload_fraction: u8,
+    /// Must be 32
+    pub minimum_embedded_payload_fraction: u8,
+    /// Must be 32
+    pub leaf_payload_fraction: u8,
+    pub file_change_counter: u32,
+    /// Size of database file in pages.
+    pub in_header_database_size: u32,
+    /// Page number of the first freelist trunk page
+    pub freelist_page_idx: u32,
+    /// Total number of freelist pages
+    pub freelist_page_count: u32,
+    /// The schema cookie
+    pub cookie: u32,
+    /// The schema format number.
+    ///
+    /// Supported schema formats are 1, 2, 3, and 4.
+    pub format_number: u32,
+    /// The default page cache-size
+    pub page_cache_size: u32,
+    /// The page number of the largest root b-tree page when in
+    /// auto-vacuum or incremental-vacuum modes, zero otherwise.
+    pub largest_root_page_idx: u32,
+    /// The database text encoding.
+    ///
+    /// A value of 1 means UTF-8.
+    /// A value of 2 means UTF-16LE.
+    /// A value of 3 means UTF-16BE.
+    pub text_encoding: u32,
+    /// The "user version" as read and set by the user_version pragma
+    pub user_version: u32,
+    /// True (non-zero) for incremental-vacuum mode.
+    /// Fales (zero) otherwise.
+    pub incremental_vacuum_enabled: u32,
+    /// The "Application ID" set by the application_id pragma
+    pub application_id: u32,
+    /// Reserved for expansion. Must be zero.
+    pub _reserved: [u8; HEADER_RESERVED_SIZE],
+    pub version_valid_for: u32,
+    pub sqlite_version_number: u32,
+}
+fn read_database_header<R: io::Read>(r: &mut R) -> io::Result<DatabaseHeader> {
+    let mut buf = [0; 100];
+    io::Read::read_exact(r, &mut buf)?;
+    let header: DatabaseHeader = unsafe { core::mem::transmute(buf) };
+    Ok(header)
+}
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -15,19 +104,21 @@ fn main() -> Result<()> {
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
+            let mut file = fs::File::open(&args[1])?;
+            let header = read_database_header(&mut file)?;
+            eprintln!("Read header {header:#?}");
+            // let mut header = [0; 100];
+            // file.read_exact(&mut header)?;
 
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
+            // #[allow(unused_variables)]
+            // let page_size = u16::from_be_bytes([header[16], header[17]]);
 
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             println!("Logs from your program will appear here!");
 
             // Uncomment this block to pass the first stage
-            // println!("database page size: {}", page_size);
+            println!("database page size: {}", header.page_size);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
